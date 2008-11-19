cab644a30c04b4d104245345e88425c73b6849d6
Camron Flanders <camron.flanders@gmail.com>
2008-11-18 22:21:27 -0600

    urls are picky, removing last slash

--- mango/urls.py
+++ mango/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 import mango.views as mango_views
 
 urlpatterns = patterns('',
-    url(r'(?P<tape_slug>[\w-]{5,50})/xspf.xml/$', mango_views.track_list, {'xspf':True,}, name="track-list-xspf"),
+    url(r'(?P<tape_slug>[\w-]{5,50})/xspf.xml$', mango_views.track_list, {'xspf':True,}, name="track-list-xspf"),
     url(r'(?P<tape_slug>[\w-]{5,50})/$', mango_views.track_list, name="track-list"),
     url(r'$',                            mango_views.tape_list, name="tape-list"),
 )
